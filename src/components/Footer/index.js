66440f44a3f1ccd730183ec361139314598494a2
ZMAN <etta1055@gmail.com>
2021-06-23 11:04:44 -0500

    changed footer icons and links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,11 +6,11 @@ function Footer() {
   const icons = [
     {
       name: "fab fa-github",
-      link: "https://github.com/"
+      link: "https://github.com/ZachAH"
     },
     {
       name: "fab fa-linkedin",
-      link: "https://www.linkedin.com/"
+      link: "https://www.linkedin.com/in/zach-howell-189118210/"
     },
   ]
 
