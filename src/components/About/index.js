3ba7b8c7c4b638dee48a6352f3e9afa7ff24092a
ZMAN <etta1055@gmail.com>
2021-06-23 11:46:04 -0500

    added email address to about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,6 +8,7 @@ function About() {
         <div className="profile-img my-5">
         {/* <i className="fas fa-user-circle"  style={{ fontSize: "96px" }}></i> */}
         <img  className="profile"src="/images/profile.jpg" alt="cute guy in chair"></img>
+          <h2 className="yahoo" onClick={() => window.open("mailto:zacharyWD91@yahoo.com")}>zacharywd91@yahoo.com</h2>
         </div>
         <p>
         I recently graduated the certificate program from UWM Extended Campus. This was a full stack bootcamp utilizing the MERN stack. Immediately after graduation I started and completed a 60 hour react certificate course. I am currently enrolled in another course because, lets face it learning in web development never ends! I am trying to find my first career into web development which is proving to be as hard or even harder then the bootcamp itself(I know hard to believe..right?!)
